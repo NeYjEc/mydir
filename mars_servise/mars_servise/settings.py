acfcda38feb1aae81268377180c8088987797f8e
NeYjEc <kacmardenis069@gmail.com>
2023-07-09 14:05:43 +0300

    connect db(ney_db)

--- mars_servise/mars_servise/settings.py
+++ mars_servise/mars_servise/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mars_servise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ney_db',
+        'USER': 'postgres',
+        'PASSWORD': '12341234D',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
